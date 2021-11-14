f5651fadd02aec626f4f83ccffeda37c49a2d845
zaya-a <rekaprekopcsak@gmail.com>
2021-11-14 22:55:45 +0100

    meh

--- js/halado.js
+++ js/halado.js
@@ -1,5 +1,14 @@
 window.addEventListener("click",myInit, false)
+var head = false;
 
 function $(id){
     return document.getElementById(id);
+}
+function myInit(){
+    $("eltunHead").style.visibility="hidden";
+    $("headclick").addEventListener("click",eltun,false)
+}
+function eltun(){
+    $("eltunHead").style.visibility="visibly";
+    console.log("asd")
 }
