895f3ee62e191eddeea54902eb6fe58c37e85dfb
zaya-a <rekaprekopcsak@gmail.com>
2021-11-15 19:05:31 +0100

    javitas&bodyszinez

--- js/halado.js
+++ js/halado.js
@@ -1,11 +1,14 @@
 window.addEventListener("load",myInit,false);
 var head = false;
+var body = false;
 function $(id){
     return document.getElementById(id);
 }
 function myInit(){
     $("eltunHead").style.display="none";
-    $("headclick").addEventListener("click",eltun,false)
+    $("eltunBody").style.display="none";
+    $("headclick").addEventListener("click",eltun,false);
+    $("bodyclick").addEventListener("click",bodytun,false);
 }
 function eltun(){
     if(head){
@@ -14,5 +17,14 @@ function eltun(){
     }else{
         $("eltunHead").style.display="block";
         head=true;
+    }
+}
+function bodytun(){
+    if(body){
+        $("eltunBody").style.display="none";
+        body=false;
+    }else{
+        $("eltunBody").style.display="block";
+        body=true;
     }
 }
